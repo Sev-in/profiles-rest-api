e47776abdb4a29bee6fa49dace8913d1cb8c0f8d
Şevin Sönmez <sevinsonmezz@gmail.com>
2025-10-17 13:07:39 +0300

    Create our user database model

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
-# Create your models here.
+class UserProfile(AbstractBaseUser, PermissionsMixin): #Inherits
+    """Database model for users in the system"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email' #This line tells Django which field to use for authentication (login).
+    REQUIRED_FIELDS = ['name'] #So with this setting you are saying: "When creating an admin, it is not enough to ask for email and password, you must also ask for the name field."
+
+    def get_full_name(self):
+        """Retrive full name of user"""
+        return self.name
+    
+    def get_short_name(self):
+        """Retrive short name of user"""
+        return self.name
+    
+    def __str__(self):
+        """Return string representation of our user"""
+        return self.email
+
