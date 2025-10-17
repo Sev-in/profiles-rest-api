6e97e48b0adc0f0b5b884438ba94f5b995850f28
Şevin Sönmez <sevinsonmezz@gmail.com>
2025-10-17 14:44:05 +0300

    Enable Django Admin

--- profiles_api/admin.py
+++ profiles_api/admin.py
@@ -1,3 +1,7 @@
-from django.contrib import admin
+from django.contrib import admin #It includes Django's admin site-related tools and functions into your project.
+
+from profiles_api import models 
+
+admin.site.register(models.UserProfile) #Get the UserProfile model and display it on my admin site
 
 # Register your models here.
